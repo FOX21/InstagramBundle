dcc2b5de433e0e1ee0e2584aa348e96d95a3afc0
Christian Mayer <christian@fox21.at>
2018-07-01 18:40:19 +0200

    Use ABSOLUTE_URL to generate Callback URL.

--- Adapter/InstaphpAdapter.php
+++ Adapter/InstaphpAdapter.php
@@ -5,6 +5,7 @@ namespace Oh\InstagramBundle\Adapter;
 use Instaphp\Instaphp;
 use Instaphp\Instagram\Response;
 use Instaphp\Exceptions\InstaphpException;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Routing\RouterInterface;
 use Oh\InstagramBundle\TokenHandler\TokenHandlerInterface;
 use Monolog\Logger;
@@ -54,7 +55,7 @@ class InstaphpAdapter extends Instaphp
 
 		// If a router is passed, generate the redirect url from the route
 		if ($router) {
-			$config['redirect_uri'] = $router->generate($config['redirect_route'], array(), true);
+			$config['redirect_uri'] = $router->generate($config['redirect_route'], array(), UrlGeneratorInterface::ABSOLUTE_URL);
 		}
 
 		$this->config = $config + $defaults;
